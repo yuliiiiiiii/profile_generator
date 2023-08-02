518599afb09b10a7826c2d10106024a1e32407f9
Yuli Wang <yuliwang1007@gmail.com>
2023-08-02 01:33:14 -0400

    created survey using readline

---
+++ survey.js
@@ -0,0 +1,28 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('What\'s your name? Nicknames are also acceptable :)', (answer1) => {
+  rl.question('What\'s an activity you like doing?', (answer2) => {
+    rl.question('What do you listen to while doing that?', (answer3) => {
+      rl.question('Which meal is your favourite (eg: dinner, brunch, etc.)', (answer4) => {
+        rl.question('What\'s your favourite thing to eat for that meal?', (answer5) => {
+          rl.question('Which sport is your absolute favourite?', (answer6) => {
+            rl.question('What is your superpower? In a few words, tell us what you are amazing at!', (answer7) => {
+              
+              console.log(`${answer1} loves listening to ${answer3} while ${answer2}, devouring ${answer5} for ${answer4}, prefers ${answer6} over any other sport, and is amazing at ${answer7}.`);
+              
+              rl.close();
+            })
+          })
+        })
+      })
+    })
+  })
+});
+
+
+
